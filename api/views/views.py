2061d0ff5009193aa1b37fdfed66971c3d26c2bf
Kevin Bae <kevinbae15@gmail.com>
2021-10-04 18:13:04 -0400

    update insertion logic + add error handling

--- api/views/views.py
+++ api/views/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework import status
 from api.serializers import *
 from api.models import *
+from django.db import transaction
 import json
 import copy
 import datetime as datetimemod
@@ -11,64 +12,95 @@ import logging
 
 logger = logging.getLogger("ten.server")
 
-
 class products(generics.ListCreateAPIView):
     serializer_class = Products
 
     def get(self, request, *arg, **kwargs):
         querySet = Products.objects.all()
-        data = self.formatProductObject(ProductsSerializer(querySet, many=True).data)
+        serializedData = ProductsSerializer(querySet, many=True).data
     
-        return Response({"status": "success", "data": data}, status=200)
-
-    def formatProductObject(self, data):
-        for product in data:
-            attributeDictionary = {}
-            for attribute in product['attributes']:
-                attributeName = attribute['attributes']['name']
-                optionName = attribute['options']['name']
-                if attributeName in attributeDictionary:
-                    attributeDictionary[attributeName]['options'].append(optionName)
-                else:
-                    attributeDictionary[attributeName] = {
-                        "name": attributeName,
-                        "options": [optionName]
-                    }
-
-            product['attributes'] = []
-            for attribute in attributeDictionary:
-                 product['attributes'].append(attributeDictionary[attribute])
-
-        return data
+        return Response({"status": "success", "data": serializedData}, status=200)
 
     def post(self, request, *args, **kwargs):
         dataObject = json.loads(request.body.decode("utf-8"))
 
-        productObject = self.insertProduct(dataObject['name'])
-        self.insertAttributesOptions(dataObject['attributes'], productObject)
+        try:
+            with transaction.atomic():
+                productObject = self.insertProduct(dataObject)
+        except ValueError as error:
+            return Response({"status": "failure", "errorMessage": str(error)}, status=400)
+        except Exception as error:
+            logger.info(repr(error));
+            return Response({"status": "error", "errorMessage": "Something went wrong"}, status=500)
 
         return Response({"status": "success", "data": []}, status=200)
 
-    def insertProduct(self, name):
-        productObject = Products(name = name)
+    '''
+        Params: 
+            - product with valid "name"
+            - product with list of attributes is optional
+    '''
+    def insertProduct(self, product):
+        if not 'name' in product or not product['name']:
+            raise ValueError("Product name cannot be empty")
+
+        if len(product['name']) > 255:
+            raise ValueError("Product name must be less than 255 characters")
+
+        productObject = Products(name = product['name'])
         productObject.save()
+
+        if 'attributes' in product:
+            self.insertAttributes(product['attributes'], productObject)
 
         return productObject
 
-    def insertAttributesOptions(self, attributes, productObject):
+    '''
+        Params: 
+            - attributes with valid "name"
+            - attributes with list of options
+            - valid Products object
+    '''
+    def insertAttributes(self, attributes, productObject):
+        if not productObject:
+            raise TypeError("insertAttributes: productObject is not valid")
+
+        attributeSet = set()
+
         for attribute in attributes:
-            logger.info("attribute name: {} options: {}".format(attribute['name'], attribute['options']))
-            attributeObject = Attributes(name = attribute['name'])
+            if not 'name' in attribute or not attribute['name']:
+                raise ValueError("Attribute name cannot be empty")
+
+            if attribute['name'] in attributeSet:
+                raise ValueError("Cannot have duplicate attributes for a single product")
+
+            if not 'options' in attribute:
+                raise ValueError("Options are required to assign to attribute")
+
+            attributeObject = Attributes(name = attribute['name'], products = productObject)
             attributeObject.save()
+            attributeSet.add(attribute['name'])
 
-            for option in attribute['options']:
-                optionObj = Options(name = option)
-                optionObj.save()
+            self.insertOptions(attribute['options'], attributeObject)
 
-                attributesOptionsMappingObject = AttributesOptionsMapping(
-                    products = productObject,
-                    attributes = attributeObject,
-                    options = optionObj
-                )
+    '''
+        Params: 
+            - options with valid "name"
+            - valid Attributes object
+    '''
+    def insertOptions(self, options, attributeObject):
+        if not attributeObject:
+            raise TypeError("insertAttributes: attributeObject is not valid")
 
-                attributesOptionsMappingObject.save()
+        optionSet = set()
+
+        for option in options:
+            if not "name" in option or not option['name']:
+                raise ValueError("Option name cannot be empty")
+
+            if option['name'] in optionSet:
+                raise ValueError("Cannot have duplicate options for a single attribute")
+
+            optionObj = Options(name = option['name'], attributes = attributeObject)
+            optionObj.save()
+            optionSet.add(option['name'])
